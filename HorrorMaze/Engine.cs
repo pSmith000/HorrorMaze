1bd347998ca55e19ee1912de879f8a0c4ec12d8a
Peyton <peytonsemail2000@gmail.com>
2021-11-14 23:28:38 -0600

    Optimized Wall Code by making Wall Class

--- HorrorMaze/Engine.cs
+++ HorrorMaze/Engine.cs
@@ -16,6 +16,7 @@ namespace HorrorMaze
         private Stopwatch _stopwatch = new Stopwatch();
         private Camera _camera;
         Player player;
+        Scene scene = new Scene();
 
         /// <summary>
         /// Called to begin the application
@@ -60,51 +61,24 @@ namespace HorrorMaze
         {
             _stopwatch.Start();
             //Create a window using raylib
-            Raylib.InitWindow(1920, 1080, "Math for Games");
-            //Raylib.InitWindow(900, 500, "Math for Games");
+            //Raylib.InitWindow(1920, 1080, "Math for Games");
+            Raylib.InitWindow(900, 500, "Math for Games");
             Raylib.SetTargetFPS(60);
 
-            Initialize();
+            InitializeCharacters();
+            InitializeWalls();
 
             _scenes[_currentSceneIndex].Start();
         }
 
-        public void Initialize()
+        public void InitializeCharacters()
         {
-
-            Scene scene = new Scene();
-
-            player = new Player(0, 0, -90, 100, "Player", Shape.SPHERE);
+            player = new Player(0, 0, -90, 20, "Player", Shape.SPHERE);
             _camera = new Camera(player);
             player.SetScale(1, 1, 1);
             CircleCollider playerCircleCollider = new CircleCollider(1, player);
             player.SetColor(new Vector4(10, 20, 200, 255));
             player.AddChild(_camera);
-
-            Actor wall = new Actor(0, -2, 0, "Wall");
-            wall.SetScale(200, 1, 200);
-            wall.SetColor(new Vector4(0, 0, 0, 255));
-
-            Actor wall2 = new Actor(0, -2, 100, "Wall");
-            wall2.SetScale(200, 50, 2);
-
-            Actor wall3 = new Actor(0, -2, -100, "Wall");
-            wall3.SetScale(200, 50, 2);
-
-            Actor wall4 = new Actor(100, -2, 0, "Wall");
-            wall4.SetScale(2, 50, 200);
-
-            Actor wall5 = new Actor(-100, -2, 0, "Wall");
-            wall5.SetScale(2, 50, 200);
-
-            Actor wall6 = new Actor(10, -2, -90, "Wall");
-            wall6.SetScale(2, 50, 20);
-
-            Actor wall7 = new Actor(-10, -2, -90, "Wall");
-            wall7.SetScale(2, 50, 80);
-
-            Actor wall8 = new Actor(34, -2, -80, "Wall");
-            wall8.SetScale(50, 50, 2);
 
             Enemy enemy = new Enemy(50, 1, 50, 10, player, "Enemy", Shape.CUBE);
             enemy.SetTranslation(50, 1, 50);
@@ -120,41 +94,44 @@ namespace HorrorMaze
             enemyEye1.SetScale(1, 1, 1);
             enemyEye1.SetColor(new Vector4(0, 0, 10, 255));
             enemyHead.AddChild(enemyEye1);
-            AABBCollider wallCollider = new AABBCollider(200, 1, 200, wall);
-            AABBCollider wall2Collider = new AABBCollider(200, 50, 2, wall2);
-            AABBCollider wall3Collider = new AABBCollider(200, 50, 2, wall3);
-            AABBCollider wall4Collider = new AABBCollider(2, 50, 200, wall4);
-            AABBCollider wall5Collider = new AABBCollider(2, 50, 200, wall5);
-            AABBCollider wall6Collider = new AABBCollider(2, 50, 20, wall6);
-            AABBCollider wall7Collider = new AABBCollider(2, 50, 40, wall7);
-            AABBCollider wall8Collider = new AABBCollider(50, 50, 2, wall8);
-
+            
             player.Collider = playerCircleCollider;
-            wall.Collider = wallCollider;
-            wall2.Collider = wall2Collider;
-            wall3.Collider = wall3Collider;
-            wall4.Collider = wall4Collider;
-            wall5.Collider = wall5Collider;
-            wall6.Collider = wall6Collider;
-            wall7.Collider = wall7Collider;
-            wall8.Collider = wall8Collider;
-
+            
             scene.AddActor(player);
             scene.AddActor(enemy);
             scene.AddActor(enemyTorso);
             scene.AddActor(enemyHead);
             scene.AddActor(enemyEye1);
             scene.AddActor(_camera);
-            scene.AddActor(wall);
-            scene.AddActor(wall2);
-            scene.AddActor(wall3);
-            scene.AddActor(wall4);
-            scene.AddActor(wall5);
-            scene.AddActor(wall6);
-            scene.AddActor(wall7);
-            scene.AddActor(wall8);
-
+            
             _currentSceneIndex = AddScene(scene);
+        }
+
+        public void InitializeWalls()
+        {
+            Wall wall = new Wall(0, -2, 0, 200, 1, 200, scene);
+            wall.SetColor(new Vector4(0, 0, 0, 255));
+
+            Wall wall2 = new Wall(0, -2, 100, 200, 50, 2, scene);
+
+            Wall wall3 = new Wall(0, -2, -100, 200, 50, 2, scene);
+
+            Wall wall4 = new Wall(100, -2, 0, 2, 50, 200, scene);
+
+            Wall wall5 = new Wall(-100, -2, 0, 2, 50, 200, scene);
+
+            Wall wall6 = new Wall(10, -2, -90, 2, 50, 20, scene);
+
+            Wall wall7 = new Wall(-10, -2, -90, 2, 50, 80, scene);
+
+            Wall wall8 = new Wall(34, -2, -80, 50, 50, 2, scene);
+
+            Wall wall9 = new Wall(34, -2, -60, 50, 50, 2, scene);
+
+            Wall wall10 = new Wall(80, -2, -70, 2, 50, 30, scene);
+
+            Wall wall11 = new Wall(90, -2, -70, 20, 50, 2, scene);
+
         }
 
         /// <summary>
---
+++ HorrorMaze/Wall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HorrorMaze
+{
+    class Wall : Actor
+    {
+        public Wall(float x, float y, float z, float scaleX, float scaleY, float scaleZ, Scene scene, string name = "Wall") : 
+            base(x, y, z, name)
+        {
+            SetScale(scaleX, scaleY, scaleZ);
+            AABBCollider wallCollider = new AABBCollider(scaleX, scaleY, scaleZ, this);
+            Collider = wallCollider;
+            scene.AddActor(this);
+        }
+
+    }
+}
